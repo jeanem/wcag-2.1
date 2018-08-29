a8f31ce237e571422fe747a12962e9f9baa8c67e
Jeane Marty <jeanem@uw.edu>
2018-08-29 07:43:40 -0700

    space

--- src/slides/Overview.js
+++ src/slides/Overview.js
@@ -10,7 +10,7 @@ class Overview extends Component {
                     <li>Improves accessibility for users who have cognitive or learning disabilities</li>
                     <li>WCAG 2.0 is still a W3C Recommendation, 2.1 extends it and is backwards compatible</li>
                 </ul>
-                <p>This presentation highlights a sub-set of WCAG 2.1 checkpoints. For more information on all checkpoints see<a href="https://www.w3.org/TR/WCAG21/#new-features-in-wcag-2-1">New features in WCAG 2.1</a></p>
+                <p>This presentation highlights a sub-set of WCAG 2.1 checkpoints. For more information on all checkpoints see <a href="https://www.w3.org/TR/WCAG21/#new-features-in-wcag-2-1">New features in WCAG 2.1</a></p>
             </div>
         );
     }
