dfed36b5a39908d917c1f730dacc4d6c0922716e
Jeane Marty <jeanem@uw.edu>
2018-09-13 12:58:19 -0700

    typos

--- src/slides/ContentOnHover.js
+++ src/slides/ContentOnHover.js
@@ -9,7 +9,7 @@ function ContentOnHover(props) {
             <ul>
                 <li>User can dismiss without moving the pointer or keyboard focus - e.g. use the escape key</li>
                 <li>Hoverable - additional content does not disappear when moved over.</li>
-                <li>Persistent - is not time based, that is if the additional content has been triggered into a visible state via focus or hover, it remains visible until the user removes the hover/focus or dismisses it</li>
+                <li>Persistent - is not time based, that is if the additional content has been triggered into a visible state via focus or hover,  it remains visible until the user removes the hover/focus or dismisses it</li>
             </ul>
         </div> 
     );
