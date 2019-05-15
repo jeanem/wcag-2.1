69c87d4056b145c204e20998d94aad93861ffd1c
Jeane Marty <jeanem@uw.edu>
2019-05-14 20:40:04 -0700

    new build

---
+++ build/precache-manifest.0375554922a956ccb19404ebdf346ba2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159326e3048094099bf868229ebed030",
+    "url": "/html-slides/index.html"
+  },
+  {
+    "revision": "269d4fb28e65d61972ef",
+    "url": "/html-slides/static/css/2.89a963ad.chunk.css"
+  },
+  {
+    "revision": "d33ae83ee5e68e1d3d19",
+    "url": "/html-slides/static/css/main.550221ae.chunk.css"
+  },
+  {
+    "revision": "269d4fb28e65d61972ef",
+    "url": "/html-slides/static/js/2.a5e76771.chunk.js"
+  },
+  {
+    "revision": "d33ae83ee5e68e1d3d19",
+    "url": "/html-slides/static/js/main.eb3a819b.chunk.js"
+  },
+  {
+    "revision": "f66389c067ce12b5f593",
+    "url": "/html-slides/static/js/runtime~main.9083bb4e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/html-slides/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/html-slides/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/html-slides/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/html-slides/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/html-slides/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
